e74fa84f2c1eb11a5eeff576954d7788466e62ef
kennysk8 <50539706+kennysk8@users.noreply.github.com>
2019-09-28 22:33:56 -0500

    Add files via upload

---
+++ index.hyperesources/triviaInfo.js
@@ -0,0 +1,81 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"SKATEBOARDING TRIVIA";
+	window.questions = [		"WHO WON SKATER OF THE YEAR IN 2014", 
+								"WHO WON X GAMES IN 2015",
+								"WHO ONE STREET LEAGUE IN 2017", 
+								"WHO STARTED THE BOARD COMPANY APRIL", 
+								"WHO WON THE BAB 8",
+								"WHO WON THE MOST STREET LEAGUES",
+								"WHO WON THE MOST X GAMES GOLD MEDALS",
+								"WHO HAS THE MOST SKATE SHOES CREATED",
+								"WHO WON THE FIRST RUN AND GUN",
+								"WHO WON THE FIRST STREET LEAGUE" ];
+	window.correctAnswer = [ 	"D", "C", "A", "B", "E",]; 
+	window.answerA = [ 			"CHIRS COLE", 
+								"SEAN MALTO", 
+								"NYJAH HUSTON", 
+								"BOO JOHNSON", 
+								"CHRIS COLE",
+								"SHANE ONEIL",
+								"TOREY PUDWILL", 
+								"PAUL RODRIGUEZ", 
+								"DAVID GONZALEZ", 
+								"DAVID GRAVETTE" ];
+	window.answerB = [ 			"TYSHAWN JONES", 
+								"BRYAN HERMAN", 
+								"RYAN SHECKLER", 
+								"SHANE ONEILL",
+								"RYAN DECENZO",
+								"SHAWN WHITE",
+								"NYJAH HUSTON", 
+								"TRENTON MCCLUNG", 
+								"JACK OLSON", 
+								"SHANE ONEILL" ];
+	window.answerC = [ 			"PAUL RODRIGUEZ",
+								"KELVIN HOEFLER",
+								"FRANKY VILLANI", 
+								"ZION WRIGHT", 
+								"ANDREW REYNOLDS",
+								"NYJAH HUSTON",
+								"MILES SILVAS", 
+								"MIKEY TAYLOR", 
+								"PAUL RODRIGUEZ", 
+								"ISHOD WAIR" ];
+	window.answerD = [ 			"WES KREMER",  
+								"ISHOD WAIR",
+								"PAUL RODRIGUEZ", 
+								"MIKEY TAYLOR",
+								"MILES SILVAS",
+								"TONY HAWK",
+								"TYSHAWN JONES", 
+								"TYSON BOWERBANK", 
+								"TIAGO LEMOS", 
+								"CHAZ ORTIZ" ];
+	window.answerE = [ 			"JAMIE FOY", 
+								"FELIPE GUSTAVO", 
+								"TOMMY FYNN", 
+								"CHRIS JOSLIN", 
+								"SEWA KROETKOV",
+								"DASHWAN JORDAN",
+								"GUSTAVO RIBEIRO", 
+								"TJ ROGERS", 
+								"ALEX MIDLER", 
+								"ALEX MAJERUS" ];
+
+
+	window.response = [ 		"WES KREMER DROPPED SOME OF HIS BEST VIDEO PARTS IN 2014", 
+								"KELVIN HOEFLER WAS ABLE TO DESTROY EVERY ONE IN THAT CONTEST'", 
+								"WHEN DOESN'T HE WIN ", 
+								"ONE OF THE MOST TECH SKATEBOARDERS TO STEP ON A BOARD", 
+								"HAS EVERYONE FLAT GROUND TRICK EVERY EVENTED",
+								"NO SURPRISE WITH THIS ONE ",
+								"BEEN WINNING CONTEST SINCE HE STARTED SKATEBOARDING", 
+								"EVERYONES FAVORITE SKATER ", 
+								"WE DON'T EVEN KNOW WHAT STANCE HE IS ", 
+								"HE'S NOT KNOW FOR WINNING CONTEST BUT HE CAME AWAY WITH THIS ONE" ];
+							
+}
+						
